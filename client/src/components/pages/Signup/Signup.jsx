7cbe166a1dd8563a72f68efd86b62b1f4d812312
Piyush Manandhar <piyush.mdhr@gmail.com>
2024-10-11 12:00:45 -0400

    Frontend Login and Signup Edits

--- client/src/components/pages/Signup/Signup.jsx
+++ client/src/components/pages/Signup/Signup.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import Header from '../../../layouts/Header';
 
 const Signup = () => {
+  // const PORT = process.env.PORT;
   const [formData, setFormData] = useState({
     username: '',
     fname: '',
@@ -80,22 +82,19 @@ const Signup = () => {
       setSuccessMessage('');
 
       try {
-        const response = await fetch(
-          `${process.env.REACT_APP_API_URL}/api/signup`,
-          {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              username: formData.username,
-              firstName: formData.fname,
-              lastName: formData.lname,
-              email: formData.email,
-              password: formData.password,
-            }),
-          }
-        );
+        const response = await fetch(`http://localhost:3000/api/signup`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            username: formData.username,
+            firstName: formData.fname,
+            lastName: formData.lname,
+            email: formData.email,
+            password: formData.password,
+          }),
+        });
 
         const data = await response.json();
 
@@ -123,165 +122,169 @@ const Signup = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto p-4 bg-gray-900 shadow-md rounded-lg">
-      <form onSubmit={handleSubmit}>
-        <h1 className="text-2xl font-bold mb-6 text-center">
-          Let's Get Started!
-        </h1>
+    <>
+      <Header />
+      <div className="max-w-md mx-auto my-20 p-4 bg-gray-900 shadow-md rounded-lg">
+        <form onSubmit={handleSubmit}>
+          <h1 className="text-2xl font-bold mb-6 text-center">
+            Let's Get Started!
+          </h1>
 
-        {/* Username */}
-        <div className="mb-4">
-          <label
-            htmlFor="username"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Username
-          </label>
-          <input
-            type="text"
-            name="username"
-            placeholder="Enter Username"
-            value={formData.username}
-            onChange={handleChange}
-            className="mt-1 block w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:ring-blue-500"
-          />
-          {errors.username && (
-            <p className="text-red-500 text-sm">{errors.username}</p>
+          {/* Username */}
+          <div className="mb-4">
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              name="username"
+              placeholder="Enter Username"
+              value={formData.username}
+              onChange={handleChange}
+              className="mt-1 block w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:ring-blue-500"
+            />
+            {errors.username && (
+              <p className="text-red-500 text-sm">{errors.username}</p>
+            )}
+          </div>
+
+          {/* First Name */}
+          <div className="mb-4">
+            <label
+              htmlFor="fname"
+              className="block text-sm font-medium text-gray-700"
+            >
+              First Name
+            </label>
+            <input
+              type="text"
+              name="fname"
+              placeholder="Enter First Name"
+              value={formData.fname}
+              onChange={handleChange}
+              className="mt-1 block w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:ring-blue-500"
+            />
+            {errors.fname && (
+              <p className="text-red-500 text-sm">{errors.fname}</p>
+            )}
+          </div>
+
+          {/* Last Name */}
+          <div className="mb-4">
+            <label
+              htmlFor="lname"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Last Name
+            </label>
+            <input
+              type="text"
+              name="lname"
+              placeholder="Enter Last Name"
+              value={formData.lname}
+              onChange={handleChange}
+              className="mt-1 block w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:ring-blue-500"
+            />
+            {errors.lname && (
+              <p className="text-red-500 text-sm">{errors.lname}</p>
+            )}
+          </div>
+
+          {/* Email */}
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleChange}
+              className="mt-1 block w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:ring-blue-500"
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email}</p>
+            )}
+          </div>
+
+          {/* Password */}
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
+              className="mt-1 block w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:ring-blue-500"
+            />
+            {errors.password && (
+              <p className="text-red-500 text-sm">{errors.password}</p>
+            )}
+          </div>
+
+          {/* Confirm Password */}
+          <div className="mb-4">
+            <label
+              htmlFor="confirmPassword"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              name="confirmPassword"
+              placeholder="Repeat Password"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              className="mt-1 block w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:ring-blue-500"
+            />
+            {errors.confirmPassword && (
+              <p className="text-red-500 text-sm">{errors.confirmPassword}</p>
+            )}
+          </div>
+
+          {/* Submit Button */}
+          <div>
+            <button
+              type="submit"
+              disabled={loading}
+              className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300"
+            >
+              {loading ? 'Submitting...' : 'Continue with Email'}
+            </button>
+          </div>
+
+          {/* Success Message */}
+          {successMessage && (
+            <p className="text-green-500 text-sm mt-4">{successMessage}</p>
           )}
-        </div>
-
-        {/* First Name */}
-        <div className="mb-4">
-          <label
-            htmlFor="fname"
-            className="block text-sm font-medium text-gray-700"
-          >
-            First Name
-          </label>
-          <input
-            type="text"
-            name="fname"
-            placeholder="Enter First Name"
-            value={formData.fname}
-            onChange={handleChange}
-            className="mt-1 block w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:ring-blue-500"
-          />
-          {errors.fname && (
-            <p className="text-red-500 text-sm">{errors.fname}</p>
+          {errors.general && (
+            <p className="text-red-500 text-sm mt-4">{errors.general}</p>
           )}
-        </div>
 
-        {/* Last Name */}
-        <div className="mb-4">
-          <label
-            htmlFor="lname"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Last Name
-          </label>
-          <input
-            type="text"
-            name="lname"
-            placeholder="Enter Last Name"
-            value={formData.lname}
-            onChange={handleChange}
-            className="mt-1 block w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:ring-blue-500"
-          />
-          {errors.lname && (
-            <p className="text-red-500 text-sm">{errors.lname}</p>
-          )}
-        </div>
-
-        {/* Email */}
-        <div className="mb-4">
-          <label
-            htmlFor="email"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={formData.email}
-            onChange={handleChange}
-            className="mt-1 block w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:ring-blue-500"
-          />
-          {errors.email && (
-            <p className="text-red-500 text-sm">{errors.email}</p>
-          )}
-        </div>
-
-        {/* Password */}
-        <div className="mb-4">
-          <label
-            htmlFor="password"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={formData.password}
-            onChange={handleChange}
-            className="mt-1 block w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:ring-blue-500"
-          />
-          {errors.password && (
-            <p className="text-red-500 text-sm">{errors.password}</p>
-          )}
-        </div>
-
-        {/* Confirm Password */}
-        <div className="mb-4">
-          <label
-            htmlFor="confirmPassword"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Confirm Password
-          </label>
-          <input
-            type="password"
-            name="confirmPassword"
-            placeholder="Repeat Password"
-            value={formData.confirmPassword}
-            onChange={handleChange}
-            className="mt-1 block w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:ring-blue-500"
-          />
-          {errors.confirmPassword && (
-            <p className="text-red-500 text-sm">{errors.confirmPassword}</p>
-          )}
-        </div>
-
-        {/* Submit Button */}
-        <div>
-          <button
-            type="submit"
-            disabled={loading}
-            className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300"
-          >
-            {loading ? 'Submitting...' : 'Continue with Email'}
-          </button>
-        </div>
-
-        {/* Success Message */}
-        {successMessage && (
-          <p className="text-green-500 text-sm mt-4">{successMessage}</p>
-        )}
-        {errors.general && (
-          <p className="text-red-500 text-sm mt-4">{errors.general}</p>
-        )}
-
-        {/* Terms */}
-        <div className="mt-6 text-sm text-gray-600">
-          <p>
-            By continuing, you agree to our Terms of Service and Privacy Policy.
-          </p>
-        </div>
-      </form>
-    </div>
+          {/* Terms */}
+          <div className="mt-6 text-sm text-gray-600">
+            <p>
+              By continuing, you agree to our Terms of Service and Privacy
+              Policy.
+            </p>
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 
--- client/src/layouts/Header.jsx
+++ client/src/layouts/Header.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Logo from '../assets/img/Logo.png';
 import { FiLogIn } from 'react-icons/fi';
 
 const Header = ({ title }) => {
   return (
     <header className="p-6 bg-white shadow-md flex justify-between items-center">
-      <h1 className="text-2xl font-bold">{title}</h1>
+      {title ? (
+        <h1 className="text-2xl font-bold">{title}</h1>
+      ) : (
+        <div className="flex items-center justify-center">
+          <Link to="/">
+            <img
+              src={Logo}
+              alt="Logo"
+              className="h-10"
+            />
+          </Link>
+        </div>
+      )}
+
       <Link
         to="/login"
         className="flex items-center space-x-4"
