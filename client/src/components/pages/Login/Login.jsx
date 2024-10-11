4564454b91771283f1a910e2e8dd0679812d7a4e
Piyush Manandhar <piyush.mdhr@gmail.com>
2024-10-11 17:51:32 -0400

    removed false commit

--- client/src/components/pages/Login/Login.jsx
+++ client/src/components/pages/Login/Login.jsx
@@ -1,7 +1,154 @@
+// import React, { useState } from 'react';
+
+// import { Link } from 'react-router-dom';
+
+// const Login = () => {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [errors, setErrors] = useState({ email: '', password: '' });
+//   const [apiError, setApiError] = useState('');
+
+//   const validateEmail = (email) => {
+//     const regularExpression = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     return regularExpression.test(email);
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     let valid = true;
+//     const newErrors = { email: '', password: '' };
+
+//     if (!email) {
+//       newErrors.email = 'Please enter an email address.';
+//       valid = false;
+//     } else if (!validateEmail(email)) {
+//       newErrors.email = 'Please enter a valid email.';
+//       valid = false;
+//     }
+
+//     if (!password) {
+//       newErrors.password = 'Please enter a password.';
+//       valid = false;
+//     }
+
+//     setErrors(newErrors);
+
+//     if (valid) {
+//       // API call for login
+//       try {
+//         const response = await fetch('/login', {
+//           method: 'POST',
+//           headers: {
+//             'Content-Type': 'application/json',
+//           },
+//           body: JSON.stringify({ email, password }),
+//         });
+
+//         const data = await response.json();
+
+//         if (response.ok) {
+//           // Assuming the API returns a JWT token
+//           localStorage.setItem('token', data.token);
+//           console.log('Login successful, token saved:', data.token);
+
+//           // Redirect to dashboard or another page if needed
+//         } else {
+//           // Handle invalid credentials or other errors
+//           setApiError(data.message || 'Login failed. Please try again.');
+//         }
+//       } catch (error) {
+//         console.error('Error during login:', error);
+//         setApiError('An error occurred during login. Please try again later.');
+//       }
+//     }
+//   };
+
+//   return (
+//     <div className="max-w-md mx-auto mt-10 p-6 bg-gray-900 shadow-md rounded-md">
+//       <h1 className="text-2xl font-semibold mb-6">Nice to see you again</h1>
+//       <form onSubmit={handleSubmit}>
+//         <div className="mb-4">
+//           <label
+//             htmlFor="Email"
+//             className="block text-sm font-medium text-gray-700"
+//           >
+//             Email
+//           </label>
+//           <input
+//             type="email"
+//             name="email"
+//             placeholder="Email"
+//             value={email}
+//             onChange={(e) => setEmail(e.target.value)}
+//             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
+//           />
+//           {errors.email && (
+//             <p className="mt-2 text-sm text-red-600">{errors.email}</p>
+//           )}
+//         </div>
+//         <div className="mb-6">
+//           <label
+//             htmlFor="password"
+//             className="block text-sm font-medium text-gray-700"
+//           >
+//             Password
+//           </label>
+//           <input
+//             type="password"
+//             name="password"
+//             placeholder="Password"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
+//           />
+//           {errors.password && (
+//             <p className="mt-2 text-sm text-red-600">{errors.password}</p>
+//           )}
+//         </div>
+//         <div>
+//           <button
+//             type="submit"
+//             className="w-full py-2 px-4 bg-blue-600 text-white rounded-md shadow-sm hover:bg-blue-700"
+//           >
+//             Continue with Email
+//           </button>
+//         </div>
+//       </form>
+//       <p className="mt-4 text-sm text-gray-600">
+//         By continuing, you agree to our{' '}
+//         <a
+//           href="#"
+//           className="text-blue-600 hover:underline"
+//         >
+//           Terms of Service
+//         </a>{' '}
+//         and{' '}
+//         <a
+//           href="#"
+//           className="text-blue-600 hover:underline"
+//         >
+//           Privacy Policy
+//         </a>
+//         .
+//       </p>
+//       <span className="mt-4 block text-sm text-gray-700">
+//         New to Percent?{' '}
+//         <Link
+//           to="/signup"
+//           className="text-blue-600 hover:underline"
+//         >
+//           Register Here
+//         </Link>
+//       </span>
+//     </div>
+//   );
+// };
+
+// export default Login;
+
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import useAuthStore from '../../../store/useAuthStore.js';
-import Header from '../../../layouts/Header.jsx';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -38,7 +185,7 @@ const Login = () => {
     if (valid) {
       // Make API call to log in
       try {
-        const response = await fetch(`http://localhost:3000/api/login`, {
+        const response = await fetch(`${process.env.MONGO_URL}/login`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -63,86 +210,83 @@ const Login = () => {
   };
 
   return (
-    <>
-      <Header />
-      <div className="max-w-md mx-auto mt-10 p-6 bg-gray-900 shadow-md rounded-md">
-        <h1 className="text-2xl font-semibold mb-6">Nice to see you again</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label
-              htmlFor="Email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
-            />
-            {errors.email && (
-              <p className="mt-2 text-sm text-red-600">{errors.email}</p>
-            )}
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
-            />
-            {errors.password && (
-              <p className="mt-2 text-sm text-red-600">{errors.password}</p>
-            )}
-          </div>
-          <div>
-            <button
-              type="submit"
-              className="w-full py-2 px-4 bg-blue-600 text-white rounded-md shadow-sm hover:bg-blue-700"
-            >
-              Continue with Email
-            </button>
-          </div>
-        </form>
-        <p className="mt-4 text-sm text-gray-600">
-          By continuing, you agree to our{' '}
-          <a
-            href="#"
-            className="text-blue-600 hover:underline"
+    <div className="max-w-md mx-auto mt-10 p-6 bg-gray-900 shadow-md rounded-md">
+      <h1 className="text-2xl font-semibold mb-6">Nice to see you again</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label
+            htmlFor="Email"
+            className="block text-sm font-medium text-gray-700"
           >
-            Terms of Service
-          </a>{' '}
-          and{' '}
-          <a
-            href="#"
-            className="text-blue-600 hover:underline"
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
+          />
+          {errors.email && (
+            <p className="mt-2 text-sm text-red-600">{errors.email}</p>
+          )}
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700"
           >
-            Privacy Policy
-          </a>
-          .
-        </p>
-        <span className="mt-4 block text-sm text-gray-700">
-          New to Percent?{' '}
-          <Link
-            to="/signup"
-            className="text-blue-600 hover:underline"
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
+          />
+          {errors.password && (
+            <p className="mt-2 text-sm text-red-600">{errors.password}</p>
+          )}
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="w-full py-2 px-4 bg-blue-600 text-white rounded-md shadow-sm hover:bg-blue-700"
           >
-            Register Here
-          </Link>
-        </span>
-      </div>
-    </>
+            Continue with Email
+          </button>
+        </div>
+      </form>
+      <p className="mt-4 text-sm text-gray-600">
+        By continuing, you agree to our{' '}
+        <a
+          href="#"
+          className="text-blue-600 hover:underline"
+        >
+          Terms of Service
+        </a>{' '}
+        and{' '}
+        <a
+          href="#"
+          className="text-blue-600 hover:underline"
+        >
+          Privacy Policy
+        </a>
+        .
+      </p>
+      <span className="mt-4 block text-sm text-gray-700">
+        New to Percent?{' '}
+        <Link
+          to="/signup"
+          className="text-blue-600 hover:underline"
+        >
+          Register Here
+        </Link>
+      </span>
+    </div>
   );
 };
 
--- src/store/useNavbarStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand';
-
-const useNavbarStore = create((set) => ({
-  activePage: 'Overview',
-  setActivePage: (page) => set({ activePage: page }),
-}));
-
-export default useNavbarStore;
