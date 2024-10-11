83a46b442129a92af42b7a190b2418182bfb5c27
Saurav Shrestha <sauravshrestha28@gmail.com>
2024-10-11 14:59:09 -0400

    Added login and signup using JWT, added error handling in Singup

--- server/helpers/errorHandler.js
+++ server/helpers/errorHandler.js
@@ -7,9 +7,13 @@ const errorController = {
 			email: '',
 			password: '',
 		};
-
 		if (err.code === 11000) {
-			errors.email = 'Email provided is already registered';
+			if (Object.keys(err.keyValue)[0] === 'email') {
+				errors.email = 'Email is already registered';
+			}
+			if (Object.keys(err.keyValue)[0] === 'username') {
+				errors.username = 'Username is already registered';
+			}
 			return errors;
 		}
 		if (err.message.includes('User validation failed')) {
